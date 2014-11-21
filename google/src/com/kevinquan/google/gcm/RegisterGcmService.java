31304e53066845f6493394709b0b03e31628b803
Kevin Quan <kevin.quan@gmail.com>
2014-11-21 17:56:52 -0500

    Prevent registering if no network

--- google/src/com/kevinquan/google/gcm/RegisterGcmService.java
+++ google/src/com/kevinquan/google/gcm/RegisterGcmService.java
@@ -5,16 +5,18 @@ import android.content.Intent;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.kevinquan.android.utils.NetworkUtils;
+
 public class RegisterGcmService extends IntentService {
 
 	private static final String TAG = RegisterGcmService.class.getSimpleName();
-	
+
 	protected static final String EXTRA_SENDER_ID = "ExtraSenderId";
-	
+
 	public RegisterGcmService() {
 		super(TAG);
 	}
-	
+
 	protected RegisterGcmService(String name) {
 		super(name);
 	}
@@ -22,13 +24,17 @@ public class RegisterGcmService extends IntentService {
 	protected Gcm getGcmHelper() {
 		return new Gcm();
 	}
-	
+
 	protected String getSenderId(Intent intent) {
 		return intent != null ? intent.getStringExtra(EXTRA_SENDER_ID) : null;
 	}
-	
+
 	@Override
 	protected void onHandleIntent(Intent intent) {
+		if (!NetworkUtils.hasNetworkConnection(this)) {
+			Log.d(TAG, "Not attempting to register GCM as there is no network connection.");
+			return;
+		}
 		String senderId = getSenderId(intent);
 		if (TextUtils.isEmpty(senderId)) {
 			Log.w(TAG, "Not registering for GCM as No sender ID provided");
