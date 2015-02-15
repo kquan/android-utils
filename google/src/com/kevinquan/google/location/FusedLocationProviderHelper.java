cacc64190401f84045459a5357ff539d6b5b548f
Kevin Quan <kevin.quan@gmail.com>
2015-02-15 18:36:21 -0500

    Update to latest google play API

--- google/src/com/kevinquan/google/location/FusedLocationProviderHelper.java
+++ google/src/com/kevinquan/google/location/FusedLocationProviderHelper.java
@@ -6,9 +6,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
-import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
-import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
 import com.kevinquan.google.PlayServicesHelper;
 
 /**
@@ -17,37 +18,39 @@ import com.kevinquan.google.PlayServicesHelper;
  *
  */
 public class FusedLocationProviderHelper {
-	
-	private static final String TAG = FusedLocationProviderHelper.class.getSimpleName();
 
 	public interface LocationRetrievedListener {
 	    void onLocationRetrieved(Location lastLocation);
 	}
 
 	public static boolean getCurrentLocation(Context context, final LocationRetrievedListener listener) {
+		if (context == null) {
+			return false;
+		}
 	    if (!PlayServicesHelper.hasUseableGooglePlayServices(context)) {
 	        Log.w(TAG, "Google Play is not available.  Cannot return current location.");
 	        return false;
 	    }
-	    final LocationClient client = new LocationClient(context, new ConnectionCallbacks() {
-	                @Override public void onConnected(Bundle arg0) {}
-	                @Override public void onDisconnected() {}
-	            }, 
-	            new OnConnectionFailedListener() {
-	                @Override public void onConnectionFailed(ConnectionResult arg0) { Log.d(TAG, "LocationClient connection failed"); }
-	            });
+	    final GoogleApiClient client = new GoogleApiClient.Builder(context).addApi(LocationServices.API)
+											.addOnConnectionFailedListener(new OnConnectionFailedListener() {
+									                @Override public void onConnectionFailed(ConnectionResult arg0) { Log.w(TAG, "Google Location Services connection failed"); }
+											})
+											.build();
 	    client.registerConnectionCallbacks(new ConnectionCallbacks() {
-	                @Override public void onDisconnected() { Log.d(TAG, "LocationClient disconnected"); }
-	                @Override public void onConnected(Bundle arg0) {
-	                    Log.d(TAG, "LocationClient connected, requesting location updates");
-	                    Location lastLocation = client.getLastLocation();
-	                    if (listener != null && lastLocation != null) {
-	                        listener.onLocationRetrieved(lastLocation);
-	                    }
-	                }
-	            });
+			@Override public void onConnected(Bundle arg0) {
+                Log.d(TAG, "LocationClient connected, requesting location updates");
+				Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(client);
+                if (listener != null && lastLocation != null) {
+                    listener.onLocationRetrieved(lastLocation);
+                }
+			}
+
+			@Override public void onConnectionSuspended(int arg0) {}
+		});
 	    client.connect();
 	    return true;
 	}
+
+	private static final String TAG = FusedLocationProviderHelper.class.getSimpleName();
 
 }
